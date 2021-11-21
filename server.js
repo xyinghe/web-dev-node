87e76e6d8b26bb44e6bae074c48e343615d3ca8f
xyinghe <he.xuey@northeastern.edu>
2021-11-21 20:26:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,5 +21,6 @@ app.get('/hello', (req, res) => {
 require('./services/movies-service')(app);
 require('./services/tweeter-service')(app);
 require('./services/profile-service')(app);
-app.listen(4000);
+
+app.listen(process.env.PORT || 4000 );
 
